47e919625f5a0245ee68680dc56fd3b59f72f279
Luis <luis.gustavo.fariac@gmail.com>
2024-06-01 09:10:59 -0300

    mock CartProvider ao em vez de CartContext.Provider

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,27 +1,38 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import Header from '../../src/components/Header/Header'
 import '@testing-library/jest-dom'
-import { CartProvider, CartContext } from '@/Hooks/useCart'
+import { CartProvider, CartContextType, CartContext } from '@/Hooks/useCart'
 
+// Mock implementation for useCart to use the mockToggleCart function
 const mockToggleCart = jest.fn()
 
-const mockCartContextValue = {
-  cartItems: [],
-  hiddenCart: true,
-  toggleCart: mockToggleCart,
-  addToCart: jest.fn(),
-  increaseQuantity: jest.fn(),
-  decreaseQuantity: jest.fn(),
-  removeFromCart: jest.fn(),
-  clearCart: jest.fn(),
-}
+jest.mock('@/Hooks/useCart', () => ({
+  ...jest.requireActual('@/Hooks/useCart'),
+  CartProvider: ({ children }: { children: React.ReactNode }) => {
+    const mockContextValue: CartContextType = {
+      cartItems: [],
+      addToCart: jest.fn(),
+      increaseQuantity: jest.fn(),
+      decreaseQuantity: jest.fn(),
+      removeFromCart: jest.fn(),
+      hiddenCart: true,
+      toggleCart: mockToggleCart,
+      clearCart: jest.fn(),
+    }
+    return (
+      <CartContext.Provider value={mockContextValue}>
+        {children}
+      </CartContext.Provider>
+    )
+  },
+}))
 
 describe('Header', () => {
   test('should render', () => {
     render(
-      <CartContext.Provider value={mockCartContextValue}>
+      <CartProvider>
         <Header />
-      </CartContext.Provider>
+      </CartProvider>
     )
 
     expect(screen.getByTestId('Title01')).toBeInTheDocument()
@@ -29,9 +40,9 @@ describe('Header', () => {
 
   test('should call toggleCart when CartButton is clicked', () => {
     render(
-      <CartContext.Provider value={mockCartContextValue}>
+      <CartProvider>
         <Header />
-      </CartContext.Provider>
+      </CartProvider>
     )
 
     const cartButton = screen.getByTestId('CartButton')
