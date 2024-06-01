9b9be14e51b7c4235fadeca10e33c33d60b380d2
Luis <luis.gustavo.fariac@gmail.com>
2024-06-01 10:33:43 -0300

    refatored Header.test.tsx

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,49 +1,38 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import Header from '../../src/components/Header/Header'
 import '@testing-library/jest-dom'
-import { CartProvider, CartContextType, CartContext } from '@/Hooks/useCart'
+import { CartProvider, CartContext } from '@/Hooks/useCart'
 
-// Mock implementation for useCart to use the mockToggleCart function
 const mockToggleCart = jest.fn()
 
-jest.mock('@/Hooks/useCart', () => ({
-  ...jest.requireActual('@/Hooks/useCart'),
-  CartProvider: ({ children }: { children: React.ReactNode }) => {
-    const mockContextValue: CartContextType = {
-      cartItems: [],
-      addToCart: jest.fn(),
-      increaseQuantity: jest.fn(),
-      decreaseQuantity: jest.fn(),
-      removeFromCart: jest.fn(),
-      hiddenCart: true,
-      toggleCart: mockToggleCart,
-      clearCart: jest.fn(),
-    }
-    return (
-      <CartContext.Provider value={mockContextValue}>
-        {children}
-      </CartContext.Provider>
-    )
-  },
-}))
+const mockCartContext = {
+  cartItems: [],
+  hiddenCart: true,
+  toggleCart: mockToggleCart,
+  addToCart: jest.fn(),
+  increaseQuantity: jest.fn(),
+  decreaseQuantity: jest.fn(),
+  removeFromCart: jest.fn(),
+  clearCart: jest.fn(),
+}
+
+const renderComponent = () => {
+  render(
+    <CartContext.Provider value={mockCartContext}>
+      <Header />
+    </CartContext.Provider>
+  )
+}
 
 describe('Header', () => {
   test('should render', () => {
-    render(
-      <CartProvider>
-        <Header />
-      </CartProvider>
-    )
+    renderComponent()
 
     expect(screen.getByTestId('Title01')).toBeInTheDocument()
   })
 
   test('should call toggleCart when CartButton is clicked', () => {
-    render(
-      <CartProvider>
-        <Header />
-      </CartProvider>
-    )
+    renderComponent()
 
     const cartButton = screen.getByTestId('CartButton')
 
